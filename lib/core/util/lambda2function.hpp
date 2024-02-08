776e0a5cf018c8cf5d9ea7e7e9a4a433d7f75312
Clang Robot <robot@clang>
2024-02-08 16:28:34 +0000

    :art: Committing clang-format changes

--- lib/core/util/lambda2function.hpp
+++ lib/core/util/lambda2function.hpp
@@ -1,5 +1,5 @@
-/** 
- *  @file   lambda2function.hpp 
+/**
+ *  @file   lambda2function.hpp
  *  @brief  Util for saving lambda functions into variables
  *  @author Alex Tsvetanov
  *  @date   2022-03-07
@@ -8,18 +8,15 @@
 #include <functional>
 #include <type_traits>
 
-namespace detail
-{
-    template <typename T> struct deduce_type;
+namespace detail {
+template <typename T> struct deduce_type;
 
-    template <typename RETURN_TYPE, typename CLASS_TYPE, typename... ARGS>
-    struct deduce_type<RETURN_TYPE(CLASS_TYPE::*)(ARGS...) const>
-    { 
-        using type = std::function<RETURN_TYPE(ARGS...)>; 
-    };
-}
+template <typename RETURN_TYPE, typename CLASS_TYPE, typename... ARGS>
+struct deduce_type<RETURN_TYPE (CLASS_TYPE::*)(ARGS...) const> {
+  using type = std::function<RETURN_TYPE(ARGS...)>;
+};
+} // namespace detail
 
-template < typename CLOSURE > auto wrap( const CLOSURE& fn )
-{ 
-    return typename detail::deduce_type<decltype(&CLOSURE::operator())>::type (fn);
+template <typename CLOSURE> auto wrap(const CLOSURE &fn) {
+  return typename detail::deduce_type<decltype(&CLOSURE::operator())>::type(fn);
 }
